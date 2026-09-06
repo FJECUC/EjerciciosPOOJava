3d7f963a21e46b4443de4b6ff53f9ce9b4b01050
Fernando Espinoza <76369721@continental.edu.pe>
2026-09-06 12:50:49 -0500

    clase autopart terminada

--- GT_Semana3/src/Vehiculo/App.java
+++ GT_Semana3/src/Vehiculo/App.java
@@ -1,6 +1,9 @@
 package Vehiculo;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        AutoPart au1 = new AutoPart(2312);
+        System.out.println(au1.precioInicial);
+        System.out.println(au1.getpctDepreciacion());
+        System.out.println(au1.depreciacion());
     }
 }
--- GT_Semana3/src/Vehiculo/AutoPart.java
+++ GT_Semana3/src/Vehiculo/AutoPart.java
@@ -1,6 +1,9 @@
 package Vehiculo;
 public class AutoPart extends Automovil{
-    AutoPart(){}
+    AutoPart(){
+        precioInicial = 1000;
+        this.setpctDepreciacion(0.3f);
+    }
     AutoPart(float precio_inicial){
         this.setpctDepreciacion(0.3f);
         this.precioInicial = precio_inicial;
